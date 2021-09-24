811a21ba66bbc8e014c856701ab2193260ac85f2
Lunay <201901649@estudantes.ips.pt>
2021-09-24 05:04:41 +0100

    A function that returns how many times c occurs in str

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int countEven(int arr[], int arrLength);
 
 int sumArray(int arr[], int arrLength);
+
+int countOccurences(char *str, char c);
 int main(){
     int values [] = {0, 1, 2, 7, 4, 6};
 
@@ -11,9 +14,15 @@ int main(){
     int evens = countEven(values, size);
 
     int total = sumArray(values, size);
+
+    char *str = "Algoritmos e Tipos Abstratos de Dados";
+    char character = 'a';
+    int occur = countOccurences(str, character);
+    printf("The character %c appears %d times in the string \n", character, occur);
     return EXIT_SUCCESS;
 }
 
+//Devolve o total de números pares
 int countEven(int arr[], int arrLength){
     int count = 0;
     for(int i = 0; i < arrLength; i++){
@@ -27,6 +36,7 @@ int countEven(int arr[], int arrLength){
     return count;
 }
 
+// Devolve a soma de elementos do array
 int sumArray(int arr[], int arrLength){
     int sum = 0;
 
@@ -35,4 +45,17 @@ int sumArray(int arr[], int arrLength){
     }
     printf("The sum of array elements is equal to %d \n", sum);
     return sum;
+}
+
+// Uma funçõa que devolve quantas vezes ocorre c em str
+int countOccurences(char *str, char c){
+    int len = strlen(str);
+
+    int count = 0;
+    for (int i = 0; i < len; i++){
+        if (str[i] == c){
+            count++;
+        }        
+    }
+    return count;
 }
