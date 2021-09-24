015942477482919c177fdd8c14d50c49faad18bf
Lunay <201901649@estudantes.ips.pt>
2021-09-24 04:36:22 +0100

    A function to count the even numbers and another to sum the array elements

--- main.c
+++ main.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void showAge();
-int main(){
-    printf("Hello world from WSL!! \n\n");
+int countEven(int arr[], int arrLength);
 
-    
-    
-    showAge();
+int sumArray(int arr[], int arrLength);
+int main(){
+    int values [] = {0, 1, 2, 7, 4, 6};
+
+    int size = 6;
+    int evens = countEven(values, size);
+
+    int total = sumArray(values, size);
     return EXIT_SUCCESS;
 }
 
-void showAge(){
-    int age = 0;
-    int *pointer = &age;
-
-    printf("Please, introduce your age!! \n");
-    scanf("%d", &age);
-
-    printf("Você tem %d anos de idade\n", age);
-
-    printf("The memory adress of age is %p \n", pointer);
+int countEven(int arr[], int arrLength){
+    int count = 0;
+    for(int i = 0; i < arrLength; i++){
+        if (arr[i] % 2 == 0)
+        {
+            count++;
+        }
+        
     }
+    printf("There are %d even numbers in the array \n\n", count);
+    return count;
+}
+
+int sumArray(int arr[], int arrLength){
+    int sum = 0;
+
+    for (int i = 0; i < arrLength; i++){
+        sum += arr[i];
+    }
+    printf("The sum of array elements is equal to %d \n", sum);
+    return sum;
+}
