cd4c70add22e0534b92602278c031989e6c41f45
Lunay <201901649@estudantes.ips.pt>
2021-09-24 05:33:10 +0100

    Function that checks if two strings are equal

--- main.c
+++ main.c
@@ -1,12 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 int countEven(int arr[], int arrLength);
 
 int sumArray(int arr[], int arrLength);
 
 int countOccurences(char *str, char c);
+
+bool equal(char *s1, char *s2);
 int main(){
     int values [] = {0, 1, 2, 7, 4, 6};
 
@@ -19,6 +22,13 @@ int main(){
     char character = 'a';
     int occur = countOccurences(str, character);
     printf("The character %c appears %d times in the string \n", character, occur);
+    
+
+    char *str1 = "Hello";
+    char *str2 = "Hello";
+    bool eq = equal(str1, str2);
+
+    printf("%d \n", eq);
     return EXIT_SUCCESS;
 }
 
@@ -58,4 +68,21 @@ int countOccurences(char *str, char c){
         }        
     }
     return count;
+}
+
+//Function that checks if two strings are equal
+bool equal(char *s1, char *s2){
+    int len1 = strlen(s1);
+    int len2 = strlen(s2);
+
+    if (len1 != len2){
+        return false;
+    }
+    
+    for (int i = 0; i < len1; i++)
+        if (s1[i] != s2[i]){
+            return false;
+        }
+
+    return true;    
 }
